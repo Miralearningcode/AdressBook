32083b386838382e08823fa773a218dd11b2544d
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2023-01-11 11:04:17 +0100

    added Models folder and Contact Model

---
+++ ConsoleApp/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp.Models;
+
+internal class Contact
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int PhoneNumber { get; set; } = 0;
+    public string Address { get; set; } = null!;
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till Adressboken");
+            Console.WriteLine("1.Skapa en kontakt");
+            Console.WriteLine("2.Visa alla kontakter");
+            Console.WriteLine("3.Visa en specifik kontakt");
+            Console.WriteLine("4.Ta bort en specifik kontakt");
+            Console.WriteLine("Välj ett av alternativen ovan:");
         }
     }
 }
